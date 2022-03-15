4c2c9124b413bee6985b05f913a9d1498cce0917
devrndx <dev@round-x.com>
2022-03-15 09:26:11 +0900

    change redis name

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_elasticache_subnet_group" "redis-subnet-group-rndx" {
 }
 
 resource "aws_elasticache_cluster" "redis-db-rndx" {
-  cluster_id           = "microservices-redis"
+  cluster_id           = "microservices-redis-rndx"
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
